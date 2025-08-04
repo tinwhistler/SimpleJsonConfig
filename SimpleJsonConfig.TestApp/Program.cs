7e2d8cddfd9a133fcd396f9cf15bfaee5edd047c
tinwhistler <greg@tinwhistler.com>
2025-08-04 16:40:41 -0500

    Initial push

---
+++ SimpleJsonConfig.TestApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using SimpleJsonConfig;
+
+var config = new Config("Test.json");
+
+int? maxUsers = config.GetInt("MaxUsers");
+bool? debugMode = config.GetBool("debugMode");
+string? name = config.GetString("AppName");
+long? maxMemory = config.GetLong("maxMemory");
+float? cpuLoad = config.GetFloat("cpuLoad");
+decimal? piValue = config.GetDecimal("piValue");
+Guid? sessionId = config.GetGuid("sessionId");
+short? portNumber = config.GetShort("portNumber");
+byte? retryCount = config.GetByte("retryCount");
+
+Console.WriteLine("maxUsers: " + maxUsers);
+Console.WriteLine("debugMode: " + debugMode);
+Console.WriteLine("AppName: " + name);
+Console.WriteLine("maxMemory: " + maxMemory);
+Console.WriteLine("cpuLoad: " + cpuLoad);
+Console.WriteLine("piValue: " + piValue);
+Console.WriteLine("sessionId: " + sessionId);
+Console.WriteLine("portNumber: " + portNumber);
+Console.WriteLine("retryCount: " + retryCount);
+
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
---
+++ SimpleJsonConfig/Config.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.Generic;
+using System.Text.Json;
+using System.Linq;
+using System.IO;
+using System;
+
+namespace SimpleJsonConfig
+{
+    public class Config
+    {
+        private readonly Dictionary<string, JsonElement> _values;
+
+        public Config(string path)
+        {
+            var json = File.ReadAllText(path);
+            using var doc = JsonDocument.Parse(json);
+            _values = doc.RootElement.EnumerateObject()
+                .ToDictionary(p => p.Name, p => p.Value.Clone(), StringComparer.OrdinalIgnoreCase);
+
+        }
+
+        public string? GetString(string key) =>
+            _values.TryGetValue(key, out var val) && val.ValueKind == JsonValueKind.String
+                ? val.GetString()
+                : null;
+
+        public int? GetInt(string key)
+        {
+            if (_values.TryGetValue(key, out var val) && val.TryGetInt32(out var i))
+                return i;
+
+            return null;
+        }
+
+        public bool? GetBool(string key)
+        {
+            if (_values.TryGetValue(key, out var val) &&
+                (val.ValueKind == JsonValueKind.True || val.ValueKind == JsonValueKind.False))
+            {
+                return val.GetBoolean();
+            }
+            return null;
+        }
+
+        public double? GetDouble(string key)
+        {
+            if (_values.TryGetValue(key, out var val) && val.TryGetDouble(out var d))
+                return d;
+
+            return null;
+        }
+
+        public DateTime? GetDateTime(string key)
+        {
+            if (_values.TryGetValue(key, out var val) && val.TryGetDateTime(out var dt))
+                return dt;
+
+            return null;
+        }
+
+        public long? GetLong(string key)
+        {
+            if (_values.TryGetValue(key, out var val) && val.TryGetInt64(out var l))
+                return l;
+
+            return null;
+        }
+
+        public float? GetFloat(string key)
+        {
+            if (_values.TryGetValue(key, out var val) && val.TryGetSingle(out var f))
+                return f;
+
+            return null;
+        }
+
+        public decimal? GetDecimal(string key)
+        {
+            if (_values.TryGetValue(key, out var val) && val.TryGetDecimal(out var d))
+                return d;
+
+            return null;
+        }
+
+        public Guid? GetGuid(string key)
+        {
+            if (_values.TryGetValue(key, out var val) && val.TryGetGuid(out var g))
+                return g;
+
+            return null;
+        }
+
+        // Optional cast-from-int entries:
+        public short? GetShort(string key)
+        {
+            if (_values.TryGetValue(key, out var val) && val.TryGetInt32(out var i))
+                return (short)i;
+
+            return null;
+        }
+
+        public byte? GetByte(string key)
+        {
+            if (_values.TryGetValue(key, out var val) && val.TryGetInt32(out var i))
+                return (byte)i;
+
+            return null;
+        }
+    }
+}
